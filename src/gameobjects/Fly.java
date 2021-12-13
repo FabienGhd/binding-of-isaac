317f75d8b2e14eb9c2891db2e0a1edeb6bd1aeb1
ahomery <axelhomery@hotmail.fr>
2021-12-13 09:57:21 +0100

    added Fly

--- src/gameobjects/Fly.java
+++ src/gameobjects/Fly.java
@@ -1,5 +1,17 @@
 package gameobjects;
 
-public class Fly {
+import libraries.Vector2;
+import resources.FlyInfos;
+import resources.ImagePaths;
 
+public class Fly extends Monster {
+
+	public Fly(Vector2 position) {
+		super(position);
+		setSize(FlyInfos.FLY_SIZE);
+		setImagePath(ImagePaths.FLY);
+		setSpeed(FlyInfos.FLY_SPEED);
+		setDamage(FlyInfos.FLY_DAMAGE);
+		setHealth(FlyInfos.FLY_HEALTH);
+	}
 }
