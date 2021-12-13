c4ccf109b1e51a2a249d755774b47f4ceed13eaa
Axel HOMERY <axelhomery@hotmail.fr>
2021-12-13 19:56:32 +0100

    movement test on Fly

--- src/gameobjects/Fly.java
+++ src/gameobjects/Fly.java
@@ -5,6 +5,8 @@ import resources.FlyInfos;
 import resources.ImagePaths;
 
 public class Fly extends Monster {
+	
+	private int count = 0; // A supprimer après
 
 	public Fly(Vector2 position) {
 		super(position);
@@ -13,5 +15,23 @@ public class Fly extends Monster {
 		setSpeed(FlyInfos.FLY_SPEED);
 		setDamage(FlyInfos.FLY_DAMAGE);
 		setHealth(FlyInfos.FLY_HEALTH);
+	}
+	
+	public void move() {
+		if(count == 0) {
+			setDirection(new Vector2(Math.random()*2-1, Math.random()*2-1));	
+			count++;			
+		}
+		else if(count < 100) {
+			count++;
+		}
+		else if(count < 120) {
+			setDirection(new Vector2());
+			count++;
+		}
+		else {count = 0;}
+		Vector2 normalizedDirection = getNormalizedDirection();
+		Vector2 positionAfterMoving = getPosition().addVector(normalizedDirection);
+		setPosition(positionAfterMoving);
 	}
 }
