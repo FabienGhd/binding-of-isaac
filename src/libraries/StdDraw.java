2b0bb644b37bd2067e1f627b72d79b2eb01786a2
FabienGhd <fabien.guihard@etudiant.univ-rennes1.fr>
2021-12-13 14:29:15 +0100

    fix window

--- src/libraries/StdDraw.java
+++ src/libraries/StdDraw.java
@@ -666,7 +666,8 @@ public final class StdDraw implements ActionListener, MouseListener, MouseMotion
 	private static void init()
 	{
 		if (frame != null)
-			frame.setVisible(false);
+//			frame.setVisible(false);
+			frame.dispose();
 		frame = new JFrame();
 		offscreenImage = new BufferedImage(2 * width, 2 * height, BufferedImage.TYPE_INT_ARGB);
 		onscreenImage = new BufferedImage(2 * width, 2 * height, BufferedImage.TYPE_INT_ARGB);
