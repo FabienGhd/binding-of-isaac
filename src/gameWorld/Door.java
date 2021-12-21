f2cac563647268f0477c2c78c7d8f31b408705cf
FabienGhd <fabien.guihard@etudiant.univ-rennes1.fr>
2021-12-22 00:52:29 +0100

    beginning of work on doors

---
+++ src/gameWorld/Door.java
@@ -0,0 +1,51 @@
+package gameWorld;
+
+import libraries.Vector2;
+
+public class Door {
+	private Room nextRoom;
+	private Vector2 position;
+	private Vector2 size;
+	private String imagePath;
+	
+	public Door(Vector2 position, Vector2 size, String imagePath, Room nextRoom) {
+		this.position = position;
+		this.size = size;
+		this.imagePath = imagePath;
+		this.nextRoom = nextRoom;
+	}
+
+	public Room getNextRoom() {
+		return nextRoom;
+	}
+
+	public void setNextRoom(Room nextRoom) {
+		this.nextRoom = nextRoom;
+	}
+
+	public Vector2 getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector2 position) {
+		this.position = position;
+	}
+
+	public Vector2 getSize() {
+		return size;
+	}
+
+	public void setSize(Vector2 size) {
+		this.size = size;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	
+
+}
