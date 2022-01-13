872307204798a170a3305b19ceea2fd50e83d6b1
FabienGhd <fabien.guihard@etudiant.univ-rennes1.fr>
2022-01-13 16:55:49 +0100

    randomization of monsters

--- src/gameWorld/MonsterRoom.java
+++ src/gameWorld/MonsterRoom.java
@@ -3,15 +3,30 @@ import gameobjects.*;
 import gameWorld.*;
 import libraries.StdDraw;
 import libraries.Vector2;
+import java.util.Random;
 
 public class MonsterRoom extends Room {
 
 	public MonsterRoom(Hero hero) {
 		super(hero);
-		
-		mobs.add(new Spider(new Vector2(0.3, 0.5), hero));
-		mobs.add(new Fly(new Vector2(0.8, 0.5), hero));
 	
+		Random random = new Random(); 
+		
+		//TODO:making sure 2 spiders don't spawn at the same position maybe. 
+		for(int i = 0; i < 3; i++) {
+			
+			int mobNbr = random.nextInt(2); //generates a random integer between 0 and 1
+			double coordinatesX = random.nextDouble(0.7)+0.2; ////generates a random double between 0.2 and 0.7 
+			double coordinatesY = random.nextDouble(0.7)+0.2;
+
+			if(mobNbr == 0) {
+				mobs.add(new Fly(new Vector2(coordinatesX, coordinatesY), hero));
+			} 	
+			else {
+				mobs.add(new Spider(new Vector2(coordinatesX, coordinatesY), hero));
+			}
+		}
+		
 	}
 	
 	public void draw() {
